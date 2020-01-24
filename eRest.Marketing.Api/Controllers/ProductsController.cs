6765d6713c2eb12f581df6a8f23f99d3be872085
Nuwan Gunawardena <NuwanGunawardena@Bloominbrands.com>
2020-01-24 17:40:14 -0500

    initial checkin

---
+++ eRest.Marketing.Api/Controllers/ProductsController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace eRest.Marketing.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private static readonly string[] Products = new[]
+        {
+            "Steak", "Fries", "Coke", "Cake"
+        };
+
+        private readonly ILogger<ProductsController> _logger;
+
+        public ProductsController(ILogger<ProductsController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+           return Products;
+        }
+    }
+}
